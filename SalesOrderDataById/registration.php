3c5d3e3d005a6045e5c5b56f4b3e4c20988fbec4
Sachin Kumar <sachinkumar@RP-sachinkumar.local>
2022-09-15 14:28:13 +0530

    Add RightpointAssignment_SalesOrderNewSection Module

---
+++ SalesOrderDataById/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'RightpointAssignment_SalesOrderDataById',
+    __DIR__
+);
